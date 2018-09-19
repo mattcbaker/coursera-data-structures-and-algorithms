5c25f4b8e984b604f3334438053d4fb830726142
mattcbaker <baker.mattc@gmail.com>
2018-09-19 09:57:03 -0400

    finish week1

---
+++ week1/max_pairwise_product.py
@@ -0,0 +1,18 @@
+# python3
+
+def max_pairwise_product(integers):
+  maximum_integers = [0] * 2
+
+  for integer in integers:
+    if integer > maximum_integers[0]:
+      maximum_integers[1] = maximum_integers[0]
+      maximum_integers[0] = integer 
+    elif integer > maximum_integers[1]:
+        maximum_integers[1] = integer
+
+  return maximum_integers[0] * maximum_integers[1]
+      
+if __name__ == '__main__':
+    input_length = int(input())
+    input_numbers = [int(x) for x in input().split()]
+    print(max_pairwise_product(input_numbers))
